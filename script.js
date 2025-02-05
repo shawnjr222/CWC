23e6d2829623466e5b26e0f4bb11dd70c058e08c
Shawn Jr <shawnmrobertsjr@gmail.com>
2025-02-04 21:57:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,30 +10,28 @@ const bgColors = [
 ];
 const bgColorElement = document.querySelector(".bg-color");
 
-// ScrollTrigger Animation
 gsap.utils.toArray(".item").forEach((item, index) => {
   let img = item.querySelector(".item-img img");
 
-  gsap.to(img, {
-    keyframes: [
-      { clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)", duration: 0 },
-      { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", duration: 2 }
-    ],
-    ease: "power1.out",
-    scrollTrigger: {
-      trigger: item,
-      start: window.innerWidth <= 450 ? "top 80%" : "center bottom",
-      end: window.innerWidth <= 450 ? "bottom top" : "bottom top",
-      toggleActions: "play none none none",
-      invalidateOnRefresh: true, // Recalculate on resize
-      markers: true,             // Debug: see trigger points
-      onEnter: () => updateBackground(bgColors[index]),
-      onEnterBack: () => updateBackground(bgColors[index]),
+  gsap.fromTo(
+    img,
+    { clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)" },
+    {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "power1.out",
+      duration: 2,
+      scrollTrigger: {
+        trigger: item,
+        start: "center bottom",
+        end: "bottom top",
+        toggleActions: "play none none none",
+        onEnter: () => updateBackground(bgColors[index]),
+        onEnterBack: () => updateBackground(bgColors[index]),
+      },
     }
-  });
+  );
 });
 
-// Background Color Update Function
 function updateBackground(color) {
   gsap.to(bgColorElement, {
     background: `linear-gradient(0deg, ${color} 0%, rgba(255, 255, 255, 0) 100%)`,
@@ -42,7 +40,6 @@ function updateBackground(color) {
   });
 }
 
-// Scroll Percentage Counter
 document.addEventListener("DOMContentLoaded", function () {
   const counterElement = document.querySelector(".counter p");
   const docHeight = document.documentElement.scrollHeight - window.innerHeight;
@@ -54,5 +51,5 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   window.addEventListener("scroll", updateScrollPercentage);
+});
 
- 
